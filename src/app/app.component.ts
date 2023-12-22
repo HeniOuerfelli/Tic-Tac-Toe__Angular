c551d6e523eae80e63f96ac03806160562afe2ae
heni-ouerfelli <132569078+Heni2002@users.noreply.github.com>
2023-12-22 03:26:57 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Tic-Tac-Toe';
+  constructor(public gameService : GameService){
+  }
+  resetGame() {
+    this.gameService.newGame();
+    }
 }
